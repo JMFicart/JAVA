9bbde756c5437c9316f6a2856f5e1e7084828a04
JMF <jm.ficart@gmail.com>
2021-11-24 23:25:28 +0100

    test

--- Exercices/ExB11.java
+++ Exercices/ExB11.java
@@ -4,7 +4,7 @@ public class ExB11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Prochain Exercice
+		// Prochain Exercice non communiqué pour le moment
 	}
 
 }
